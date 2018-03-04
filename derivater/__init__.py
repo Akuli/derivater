4cb845f93367c5a763a469488ad2e2ade3a885ce
Akuli <akuviljanen17@gmail.com>
2018-03-04 12:03:50 +0200

    updating docs with docs/publish.py

--- derivater/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-from derivater._base import (
-    eq_and_hash, mathify, MathObject, Symbol, SymbolFunction, Integer,
-    Add, Mul, Pow)
-from derivater._constants import NamedConstant, e, tau, pi
-from derivater._explog import NaturalLog, exp, ln, log, log2, log10
-from derivater._trig import sin, cos, tan
-
-__version__ = '1.0'
